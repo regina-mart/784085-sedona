b7cdada06a685a354dd400b1523648564acdabcd
regina-mart <regina-mart@mail.ru>
2018-09-09 12:41:25 +0300

    правки

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
   var link = document.querySelector(".hotel-search");
   var popup = document.querySelector(".search-form");
   var close = document.querySelector(".hotel-search-close");
-  var dateArrival = popup.querySelector(".arrival-departure-form-one");
-  var dateDeparture = popup.querySelector(".arrival-departure-form-two");
-  var form = popup.querySelector("form");
 
   link.addEventListener("click", function (evt) {
    evt.preventDefault();
@@ -13,9 +10,4 @@
   close.addEventListener("click", function (evt) {
    evt.preventDefault();
    popup.classList.toggle("search-form-show");
-});
-
-form.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-  console.log("Отправляем форму");
 });
